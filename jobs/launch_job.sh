2764e7062e6e653a0068b45ccbc466e81fd82875
Paul Szczepanek <paul.szczepanek@arm.com>
2021-06-04 15:05:53 +0100

    unique job name

--- jobs/launch_job.sh
+++ jobs/launch_job.sh
@@ -31,6 +31,7 @@ ESCAPED_USER_TOKEN=$(echo "$USER_TOKEN" | sed -e 's/[\/&]/\\&/g')
 sed -i "s/REPOSITORY=\"\"/REPOSITORY=\"${ESCAPED_REPOSITORY}\"/" ${JOB_NAME}
 sed -i "s/SHA=\"\"/SHA=\"${SHA}\"/" ${JOB_NAME}
 sed -i "s/USER_TOKEN=\"\"/USER_TOKEN=\"${ESCAPED_USER_TOKEN}\"/" ${JOB_NAME}
+sed -i "s/path: inline\/job.yaml/path: inline\/job-${SHA}.yaml/" ${JOB_NAME}
 
 rm -f ${JOB_NAME}
 
